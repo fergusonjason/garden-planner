0489b1c49a3e0613e56dadf5e448714b0450c225
Jason Ferguson <fergusonjason@gmail.com>
2026-03-14 06:52:37 -0400

    more rearranging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, inject, OnDestroy, signal, ViewEncapsulation } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
-import { PLANT_MAP } from './shared/constants/plant-map-constants';
-import { PlantDef } from './shared/models/plant-def';
-import { ExportService } from './core/services/export-service';
-import { GardenPlannerMain } from './features/garden-planner-main/components/garden-planner-main/garden-planner-main';
+import { GardenPlannerMain } from './features/garden-planner/garden-planner-main/components/garden-planner-main/garden-planner-main';
 
 
 @Component({
