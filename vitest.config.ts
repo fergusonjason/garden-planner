32f97ea07fc2ea5448fdf4526e0271cc73f4b126
Jason Ferguson <fergusonjason@gmail.com>
2026-05-29 05:16:30 -0400

    chore: vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@core': resolve(__dirname, 'src/app/core'),
+      '@shared': resolve(__dirname, 'src/app/shared'),
+      '@features': resolve(__dirname, 'src/app/features'),
+      '@environments': resolve(__dirname, 'src/environments'),
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
